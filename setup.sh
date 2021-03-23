4526f3d5ac360586c5944eeff770488eec07be4f
butterstulle93 <42576753+butterstulle93@users.noreply.github.com>
2021-03-23 22:53:42 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+eecho "\
+[general]\n\
+
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
