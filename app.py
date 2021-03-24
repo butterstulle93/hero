0bb012acaee1e83a425f2b9eb1de7fa7a782592b
butterstulle93 <42576753+butterstulle93@users.noreply.github.com>
2021-03-24 20:04:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ st.write("Hello, Heroku!")
 df = pd.read_csv("hallo.csv")
 
 st.write(df)
+
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+
+
+i = 101
+while i < 101:
+  st.write(i)
+  
+  
+ 
