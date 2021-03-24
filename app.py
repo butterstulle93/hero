0cb5267ab61e45f646e93c2c8e34e68b3bab374a
butterstulle93 <42576753+butterstulle93@users.noreply.github.com>
2021-03-24 20:06:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Hello, Heroku!")
+st.title("Mein erstes Dashboard")
 
-df = pd.read_csv("hallo.csv")
+import pydeck as pdk
 
-st.write(df)
-
-
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+HEXAGON_LAYER_DATA = (
+    "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv"  # noqa
 )
 
+# Define a layer to display on a map
+layer = pdk.Layer(
+    "HexagonLayer",
+    HEXAGON_LAYER_DATA,
+    get_position=["lng", "lat"],
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    elevation_range=[0, 3000],
+    extruded=True,
+    coverage=1,
+)
+
+# Set the viewport location
+view_state = pdk.ViewState(
+    longitude=-1.415, latitude=52.2323, zoom=6, min_zoom=5, max_zoom=15, pitch=40.5, bearing=-27.36,
+)
+
+# Render
+r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+r.to_html("hexagon_layer.html")
+
+st.pydeck_chart(r)
 
 
-i = 101
-while i < 101:
-  st.write(i)
-  
   
  
