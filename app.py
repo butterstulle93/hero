1600d2160f733b94e6aa499a7129dda750e51373
butterstulle93 <daniel-co@live.de>
2021-03-25 14:42:22 +0100

    fef

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import time
 import numpy as np
 import streamlit as st
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
+
 last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
 
@@ -11,14 +10,13 @@ for i in range(1, 101000000000000000000000000):
     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
     status_text.text("%i%% Complete" % i)
     chart.add_rows(new_rows)
-    progress_bar.progress(i)
+    
     last_rows = new_rows
     time.sleep(1)
-    st.write(i +"Versuch")
+    st.write(i + " " + "Versuch")
 
 progress_bar.empty()
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
-st.button("Re-run")
